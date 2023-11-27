cdbd13d05ec55022ab81fc97069e78e8332aff15
Willy Setiawan <2268039+willystw@users.noreply.github.com>
2023-11-27 16:09:54 +0700

    update

--- src/routes/(home)/+layout.server.js
+++ src/routes/(home)/+layout.server.js
@@ -1,14 +1,12 @@
+import { redirect } from '@sveltejs/kit';
 import 'dotenv-expand/config'
 
-export const load = async ({fetch}) => {
-    const getUser = async() => {
-        const endpoint = `${process.env.HOST_URL}/users/${process.env.USER_ID}/get`;
-        const userRes = await fetch(endpoint)
-        const userData = await userRes.json()
-
-        return userData.data
-    }
-    return {
-        user: getUser()
-    }
+/** @type {import('./$types').PageServerLoad} */
+export function load(event) {
+  if (!event.locals.user) {
+    throw redirect(302, "/login")
+  }
+	return {
+		user: event.locals.user
+	};
 }
