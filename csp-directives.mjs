a422b2bf0f31f25c55b5c43b50987f07df255c9c
Willy Setiawan <2268039+willystw@users.noreply.github.com>
2024-01-18 19:29:28 +0700

    update CSP directives

--- csp-directives.mjs
+++ csp-directives.mjs
@@ -3,7 +3,7 @@ const rootDomain = process.env.TABUNGANKU_URL; // or your server IP for dev
 const cspDirectives = {
 	'base-uri': ["'self'"],
 	'child-src': ["'self'"],
-	'connect-src': ["'self'", 'ws://localhost:*', "http://172.20.10.5:8180"],
+	'connect-src': ["'self'", 'ws://localhost:*', "http://172.20.10.5:8180", "http://192.168.18.199:8180"],
 	'img-src': ["'self'", 'data:'],
 	'font-src': ["'self'", 'data:'],
 	'form-action': ["'self'"],
@@ -11,6 +11,7 @@ const cspDirectives = {
 	'frame-src': [
 		"'self'",
     "http://172.20.10.5:8180",
+    "http://192.168.18.199:8180"
 	],
 	'manifest-src': ["'self'"],
 	'media-src': ["'self'", 'data:'],
