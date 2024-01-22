938d70d234c13c99b8992fb146556c63b2313b54
Willy Setiawan <2268039+willystw@users.noreply.github.com>
2024-01-22 15:53:32 +0700

    disable csp

--- csp-directives.mjs
+++ csp-directives.mjs
@@ -1,17 +1,15 @@
-const rootDomain = process.env.TABUNGANKU_URL; // or your server IP for dev
-
 const cspDirectives = {
 	'base-uri': ["'self'"],
 	'child-src': ["'self'"],
-	'connect-src': ["'self'", 'ws://localhost:*', "http://172.20.10.5:8180", "http://192.168.18.199:8180"],
+	'connect-src': [
+    'self', 
+  ],
 	'img-src': ["'self'", 'data:'],
 	'font-src': ["'self'", 'data:'],
 	'form-action': ["'self'"],
 	'frame-ancestors': ["'self'"],
 	'frame-src': [
-		"'self'",
-    "http://172.20.10.5:8180",
-    "http://192.168.18.199:8180"
+		'self',
 	],
 	'manifest-src': ["'self'"],
 	'media-src': ["'self'", 'data:'],
@@ -19,7 +17,6 @@ const cspDirectives = {
 	'style-src': ["'self'", "'unsafe-inline'"],
 	'default-src': [
 		'self',
-		...(rootDomain ? [rootDomain, `ws://${rootDomain}`] : []),
 	],
 	'script-src': [
 		'self',
