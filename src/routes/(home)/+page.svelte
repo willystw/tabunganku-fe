58abc25d01f1a387056eae55640b26991fd81bba
Willy Setiawan <2268039+willystw@users.noreply.github.com>
2024-02-06 17:07:20 +0700

    add next & prev date checker

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -1,18 +1,12 @@
 <script>
   export let data;
-  const { summaries } = data;
+  let { summaries } = data;
 
   import Chart from 'chart.js/auto';
   import { Colors } from 'chart.js';
   import { onMount } from 'svelte';
 
-  Chart.register(Colors);
-	let dateString;
-	
-  let portfolio;
-  let totalEarning;
-  let totalExpense;
-
+  Chart.register(Colors);	
   const chartData = {
     labels: summaries['available_categories'],
     datasets: [
@@ -57,15 +51,63 @@
           }
       }
   };
-  $: date = new Date();
-  $: dateString = date.toJSON().slice(0,10);
+  let time = new Date();
+  $: dateString = time.toJSON().slice(0,10);
+
+  async function fetchSummaries(dateData) {
+    const reqBody = {
+      date: dateData
+    };
+
+    const result = await fetch(`/api/transactions/summaries`, {
+      method: 'POST',
+      body: JSON.stringify(reqBody),
+    })
+
+    if(result.status === 200) {
+      const data = await result.json();
+      const response = {
+        summaries: data.data
+      };
+      return response;
+    } else {
+      throw new Error(`Error status ${result.status} with message: ${result.statusText}`);
+    }
+  }
+
+  async function handleDateChange() {
+    let response = await fetchSummaries(time.toJSON().slice(0,10));
+    summaries = response['summaries'];
+
+    myChart.data.labels = summaries['available_categories'];
+    myChart.data.datasets.forEach((dataset) => {
+      if(dataset.label === "Earning") {
+        dataset.data = summaries['earnings'];
+      } else if (dataset.label === "Expense") {
+        dataset.data = summaries['expenses'];
+      }
+    });
+    //update chart
+    myChart.update();
+  }
+
+  async function nextDate() {
+    time = new Date(time.setDate(time.getDate()  + 1));
+    handleDateChange();
+  }
+
+  async function prevDate() {
+    time = new Date(time.setDate(time.getDate() - 1));
+    handleDateChange();
+  }
+
+  let portfolio, myChart;
+  $: totalEarning = summaries['total_earning'];
+  $: totalExpense = summaries['total_expense'];
 
   onMount(()=> {
-      totalEarning = summaries['total_earning'].toLocaleString();
-      totalExpense = '(' + summaries['total_expense'].toLocaleString() + ')';
-
       const ctx = portfolio.getContext('2d');
-      var myChart = new Chart(ctx, config);
+      myChart = new Chart(ctx, config);
   });
 </script>
 
@@ -79,19 +121,19 @@
     <div class="dashboard">
         <div class="dashboard__item">
             <div class="card bigvalue">
-                <strong class=" bigvalue__earning" title="Earning">{totalEarning}</strong>
+                <strong class=" bigvalue__earning" title="Earning">{totalEarning.toLocaleString()}</strong>
             </div>
         </div>
         <div class="dashboard__item">
             <div class="card bigvalue">
-                <a href="#!" id="prev_date_nav" title="Previous Date">&#8249;</a>
+                <a href="#!" id="prev_date_nav" title="Previous Date" on:click={prevDate}>&#8249;</a>
                 <strong>{dateString}</strong>
-                <a href="#!" id="next_date_nav" title="Next Date">&#8250;</a>
+                <a href="#!" id="next_date_nav" title="Next Date" on:click={nextDate}>&#8250;</a>
             </div>
         </div>
         <div class="dashboard__item">
             <div class="card bigvalue">
-                <strong class="bigvalue__expense" title="Expense">{totalExpense}</strong>
+                <strong class="bigvalue__expense" title="Expense">({totalExpense.toLocaleString()})</strong>
             </div>
         </div>
         <div class="dashboard__item dashboard__item--full">
